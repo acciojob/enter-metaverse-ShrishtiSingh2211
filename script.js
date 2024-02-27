b01dc38d30256ebc10b484ee7d56846b0972cf55
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-02-27 20:24:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const para= document.getElementById("status");
+const button= document.getElementById("enterBtn");
+
+button.addEventListener("click", () => {
+	let h1 = document.createElement("h1");
+	h1.id= "status";
+	h1.innerText = "Entered Metaverse";
+	document.body.insertBefore(h1, para);
+	para.remove();
+}
