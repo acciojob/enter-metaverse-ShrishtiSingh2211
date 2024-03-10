95f4c1e5efc7006ac51189078a903a565576c00b
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-03-10 20:11:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 //your JS code here. If required.
-const para= document.getElementById("status");
-const button= document.getElementById("enterBtn");
-
-button.addEventListener("click", () => {
-	let h1 = document.createElement("h1");
-	h1.id= "status";
-	h1.innerText = "Entered Metaverse";
-	document.body.insertBefore(h1, para);
-	para.remove();
-}
+const status = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+enterBtn.addEventListener("click", () => {
+    status.outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+});
